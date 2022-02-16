031ced9ee46975dafba0d6cba30927160ef5798a
Fiona Apps <wikipanyd@gmail.com>
2022-02-16 10:58:11 +0000

    Adding router and store!

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// import ProductsList from './pages/ProductsList.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // { path: '/', redirect: '/products' },
+        // { path: '/products', component: ProductsList },
+        // { path: '/cart', component: UserCart },
+        // { path: '/admin', component: ShopAdmin },
+    ]
+});
+
+export default router;
