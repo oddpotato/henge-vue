515c55ae864c81b717229a23bc955b06c0f97955
Fiona Apps <wikipanyd@gmail.com>
2022-02-16 11:14:30 +0000

    Adding pages

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-// import ProductsList from './pages/ProductsList.vue';
+import HomePage from './pages/HomePage.vue';
+import Druids from './pages/Druids.vue';
+import ContactUs from './pages/ContactUs.vue';
+import Messages from './pages/Messages.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        // { path: '/', redirect: '/products' },
-        // { path: '/products', component: ProductsList },
-        // { path: '/cart', component: UserCart },
-        // { path: '/admin', component: ShopAdmin },
+        { path: '/', component: HomePage },
+        { path: '/druids', component: Druids },
+        { path: '/contact', component: ContactUs },
+        { path: '/messages', component: Messages },
     ]
 });
 
