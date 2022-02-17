88eb7d29c78e65944c604389924377c92de64323
Fiona Apps <wikipanyd@gmail.com>
2022-02-17 10:15:06 +0000

    Adding forloop over json so that I can make an 'edit json' page

---
+++ src/store/druids/getters.js
@@ -0,0 +1,5 @@
+export default {
+    listofdruids(state) {
+        return state.listofdruids;
+    },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from 'vuex'
+import druidsModule from './druids'
 
 export default createStore({
-
+    modules: {
+        druids: druidsModule,
+    },
 })
